6bc25e2899ae2c36659660010ea7bf689ea2b563
Lienkulet <clocicovalexandru@gmail.com>
2026-05-11 12:02:54 +0300

    test

--- MedPrestige.UI/obj/Debug/net10.0/MedPrestige.UI.AssemblyInfo.cs
+++ MedPrestige.UI/obj/Debug/net10.0/MedPrestige.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedPrestige.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9869a038504f9272e39bc12db75ec683285ba5cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d1f37013feabcde5e5ebd5b63529163af29bc4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedPrestige.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedPrestige.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
